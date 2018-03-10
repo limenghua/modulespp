b776fb031ba3b2b0d19c4fb663ca8738868dfc0a
limenghua <576566385@qq.com>
2018-03-10 11:04:41 +0800

    add example "hello world"

---
+++ examples/hello_world.cpp
@@ -0,0 +1,50 @@
+//
+// Created by limenghua on 18-3-10.
+//
+
+#include <iostream>
+#include <modulepp/application.h>
+#include <modulepp/module.h>
+
+using namespace modulepp;
+
+class hello_module:public module{
+public:
+    hello_module():
+        module("hello_module"){
+        std::cout<<"hello_module contructed"<<std::endl;
+    }
+
+    ~hello_module(){
+        std::cout<<"hello_module disctructed"<<std::endl;
+    }
+
+    virtual void start()override {
+        module::start();
+        std::cout<<"hello_module::start"<<std::endl;
+    }
+
+    virtual void stop()override {
+        module::stop();
+        std::cout<<"hello_module::stop"<<std::endl;
+    }
+};
+
+
+int main()
+{
+    std::cout<<"start run..."<<std::endl;
+
+    {
+        application app;
+        app.register_module(std::make_shared<hello_module>());
+
+        app.start();
+        //do some working...
+        app.stop();
+    }
+
+    std::cout<<"exit running"<<std::endl;
+    return 0;
+}
+
